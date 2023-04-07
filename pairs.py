1c20ba02f607a176b42ae0a32ae9f5f5a235dbfb
kumud19429@iiitd.ac.in <kumud19429@iiitd.ac.in>
2023-04-08 00:05:30 +0530

    Added orders

--- pairs.py
+++ pairs.py
@@ -33,13 +33,62 @@ class pair:
 		self.data_x2 = []
 		self.data = []
 		self.prices = {}
+		self.positions = {"Y":0,"X1":0,"X2":0}
 		self.se = 0
 		self.beta = 0
 		self.get_data()
+		self.money = 150
+		self.leverage = 30
 		self.model = self.get_model(self.data)
 		self.threshold = int(tickers[4])
 		self.ws = UMFuturesWebsocketClient()
+		self.direction = ""
 		self.subscribe_to_ws()
+
+	def buy(self,symbol,qty,leverage):
+		symbol = symbol.upper()
+		i = 2
+		qty = round(qty,i)
+		while True:
+			try:
+				response = self.client.new_order(
+					symbol = symbol,
+					side = "BUY",
+					type = "MARKET",
+					quantity = qty,
+					leverage = leverage)
+				break
+			except Exception as e:
+				if i > 0:
+					i = i-1
+					qty = round(qty,i)
+				else:
+					print(e)
+					return -1
+		return qty
+
+	def sell(self,symbol,qty,leverage):
+		symbol = symbol.upper()
+		i = 2
+		qty = round(qty,i)
+		while True:
+			print(qty)
+			try:
+				response = self.client.new_order(
+					symbol = symbol,
+					side = "SELL",
+					type = "MARKET",
+					quantity = qty,
+					leverage = leverage)
+				break
+			except Exception as e:
+				if i > 0:
+					i = i-1
+					qty = round(qty,i)
+				else:
+					print(e)
+					return -1
+		return qty
 
 	def message_handler_y(self,message):
 		if message.get('k') != None:
@@ -126,6 +175,11 @@ class pair:
 		if len(self.prices) < 3:
 			return
 		else:
+			ub = self.threshold
+			lb = - self.threshold
+			s_ub = ub/2
+			s_lb = lb/2
+
 			if self.model!=None:
 				y = self.prices["y"]
 				x1 = self.prices["x1"]
@@ -135,8 +189,98 @@ class pair:
 				trig = (y - ypred)/self.se
 				res = trig[0]
 				# print(y,x1,x2,"Z-score : ",res,"Beta : ",self.beta)
-				if abs(res) >self.threshold:
-					print(self.y,self.x1,self.x2,"Z-score : ",res,"Beta : ",self.beta)
+				# if abs(res) >self.threshold:
+				print(self.y,self.x1,self.x2,"Z-score : ",res,"Beta : ",self.beta)
+				posy,posx1,posx2 = self.positions["Y"],self.positions["X1"],self.positions["X2"]
+				if posy==0 and posx1==0 and posx2==0:
+					if res >= ub:
+						print('SHORT')
+						self.direction = "S"
+						# ratio = 1/(y + x1 + x2)
+						# qtyY = y*ratio*self.money
+						# qtyX1 = x1*ratio*self.money
+						# qtyX2 = x2*ratio*self.money
+						qtyY = (self.money/3)/y
+						qtyX1 = (self.money/3)/x1
+						qtyX2 = (self.money/3)/x2
+
+						quantity = self.sell(self.y,qtyY,self.leverage)
+						if quantity == -1:
+							return
+						self.positions["Y"] = -quantity
+						
+						quantity = self.buy(self.x1,qtyX1,self.leverage)
+						if quantity == -1:
+							return
+						self.positions["X1"] = quantity
+						# else:
+						# 	quantity = self.sell(self.x1,qtyX1,self.leverage)
+						# 	self.positions["X1"] = -quantity
+						# if self.beta["X2"] > 0:
+						
+						quantity = self.buy(self.x2,qtyX2,self.leverage)
+						if quantity == -1:
+							return
+						self.positions["X2"] = quantity
+						# else:
+						# 	quantity = self.sell(self.x2,qtyX2,self.leverage)
+						# 	self.positions["X2"] = -quantity
+					if res <= lb:
+						print('LONG')
+						self.direction = "L"
+						# ratio = 1/(y + x1 + x2)
+						# qtyY = y*ratio*self.money
+						# qtyX1 = x1*ratio*self.money
+						# qtyX2 = x2*ratio*self.money
+
+						qtyY = (self.money/3)/y
+						qtyX1 = (self.money/3)/x1
+						qtyX2 = (self.money/3)/x2
+						
+						quantity = self.buy(self.y,qtyY,self.leverage)
+						if quantity == -1:
+							return
+						self.positions["Y"] = quantity
+						
+						quantity = self.sell(self.x1,qtyX1,self.leverage)
+						if quantity == -1:
+							return
+						self.positions["X1"] = -quantity
+						# else:
+						# 	quantity = self.sell(self.x1,qtyX1,self.leverage)
+						# 	self.positions["X1"] = -quantity
+						# if self.beta["X2"] > 0:
+						
+						quantity = self.sell(self.x2,qtyX2,self.leverage)
+						if quantity == -1:
+							return
+						self.positions["X2"] = -quantity
+				else:
+					if res <= s_ub and self.direction == "S":
+						print("SQUARE OFF SHORT")
+						self.direction = ""
+						if posy < 0:
+							self.buy(self.y,-posy,self.leverage)
+							self.positions["Y"] = 0
+						if posx1>0:
+							self.sell(self.x1,posx1,self.leverage)
+							self.positions["X1"] = 0
+						if posx2>0:
+							self.sell(self.x2,posx2,self.leverage)
+							self.positions["X2"] = 0
+
+					elif res>=s_lb and self.direction == "L":
+						print("SQUARE OFF LONG")
+						self.direction = ""
+						if posy > 0:
+							self.sell(self.y,posy,self.leverage)
+							self.positions["Y"] = 0
+						if posx1<0:
+							self.buy(self.x1,-posx1,self.leverage)
+							self.positions["X1"] = 0
+						if posx2<0:
+							self.buy(self.x2,-posx2,self.leverage)
+							self.positions["X2"] = 0
 			self.get_data()
 			self.get_model(self.data)
 			self.prices = {}
